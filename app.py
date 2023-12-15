6704c631589c0d735561a7f5ad2a26e9993dd2a0
AmeliaKurnia <130240488+AmeliaKurnia@users.noreply.github.com>
2023-12-15 08:34:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,22 @@ from sqlalchemy import text
 
 list_maskapai = ['', 'Amelia Airlines', 'HaykalAir', 'AirRindah', 'Ratna Airlines', 'AbghazAir']
 list_kode = ['', 'AK009', 'FH027', 'AM088', 'RM097', 'AB100']
+list_airport = ['', 'Juanda', 'Ngurah Rai', 'Soekarno-Hatta', 'Sultan Hasanuddin', 'Adisutjipto'] 
 
 conn = st.connection("postgresql", type="sql", 
                      url="postgresql://AmeliaKurnia:AyZa67mtESxw@ep-morning-glade-50476120.us-east-2.aws.neon.tech/fp3")
+
 with conn.session as session:
     query = text('CREATE TABLE IF NOT EXISTS AIRPORT (id serial, nama_maskapai varchar, nama_pilot varchar, kode_penerbangan char(25), \
-                                                       kelas text, bandara_asal varchar, bandara_tujuan text, tanggal date);')
+                                                       kelas text, bandara_asal varchar, bandara_tujuan text, waktu time, tanggal date);')
     session.execute(query)
+
+st.set_page_config(
+    page_title="FlyTrack Group Four's Data Management System",
+    page_icon="✈️",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
 
 st.header("FlyTrack Group Four'S Data Management System")
 page = st.sidebar.selectbox("Pilih Menu", ["View Data","Edit Data"])
@@ -26,6 +35,8 @@ if page == "View Data":
     st.markdown("5. Abghaza Bayu Kusuma W. P. (2043221100)")
 
 if page == "Edit Data":
+    search_input = st.sidebar.text_input("Search", "")
+    
     if st.button('Tambah Data'):
         with conn.session as session:
             query = text('INSERT INTO airport (nama_maskapai, nama_pilot, kode_penerbangan, kelas, bandara_asal, bandara_tujuan, waktu, tanggal) \
@@ -51,9 +62,9 @@ if page == "Edit Data":
                 nama_pilot_baru = st.text_input("nama_pilot", nama_pilot_lama)
                 kode_penerbangan_baru = st.selectbox("kode_penerbangan", list_kode, list_kode.index(kode_penerbangan_lama))
                 default_kelas = eval(kelas_lama) if eval(kelas_lama) and isinstance(eval(kelas_lama), list) else []
-                kelas_baru = st.multiselect("kelas", ['economy', 'comfort', 'business', 'amatiran'], default=list(set(default_kelas) & set(['economy', 'comfort', 'business', 'amatiran'])))
-                bandara_asal_baru = st.text_input("bandara_asal", bandara_asal_lama)
-                bandara_tujuan_baru = st.text_input("bandara_tujuan", bandara_tujuan_lama)
+                kelas_baru = st.multiselect("kelas", ['economy', 'comfort', 'business', 'amatiran'], default=list(set(default_kelas) & set(['economy', 'comfort', 'business', 'premium'])))
+                bandara_asal_baru = st.selectbox("bandara_asal", list_airport, list_airport.index(bandara_asal_lama))
+                bandara_tujuan_baru = st.selectbox("bandara_tujuan", list_airport, list_airport.index(bandara_tujuan_lama))
                 waktu_baru = st.time_input("waktu", waktu_lama)
                 tanggal_baru = st.date_input("tanggal", tanggal_lama)
                 
